118043bf9ae04d40f27c904c91f85bb92c2e0cee
KHTangent <contact@khtangent.com>
2024-12-11 08:52:29 +0100

    Memoize the function instead

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,12 +1,10 @@
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use memoize::memoize;
 
-fn solution(input: &str, blinks: usize) -> i64 {
-	let mut stones: Vec<i64> = input
-		.split(" ")
-		.map(|s| s.parse::<i64>().unwrap())
-		.collect();
+#[memoize]
+fn process_stone(input: i64, blinks: usize, stones_per_recursion: usize) -> i64 {
 	let mut generations_processed = 0;
-	for _ in 0..blinks.min(40) {
+	let mut stones = vec![input];
+	for _ in 0..blinks.min(stones_per_recursion) {
 		let mut new_stones: Vec<i64> = Vec::with_capacity(stones.len() * 2);
 		for stone in stones {
 			let stone_string = stone.to_string();
@@ -25,12 +23,26 @@ fn solution(input: &str, blinks: usize) -> i64 {
 	}
 	if generations_processed < blinks {
 		stones
-			.into_par_iter()
-			.map(|stone| solution(stone.to_string().as_str(), blinks - 40))
+			.into_iter()
+			.map(|stone| {
+				solution(
+					stone.to_string().as_str(),
+					blinks - stones_per_recursion,
+					stones_per_recursion,
+				)
+			})
 			.sum::<i64>()
 	} else {
 		stones.len() as i64
 	}
+}
+
+fn solution(input: &str, blinks: usize, stones_per_recursion: usize) -> i64 {
+	input
+		.split(" ")
+		.map(|s| s.parse::<i64>().unwrap())
+		.map(|stone| process_stone(stone, blinks, stones_per_recursion))
+		.sum::<i64>()
 }
 
 #[cfg(test)]
@@ -40,22 +52,22 @@ mod tests {
 	#[test]
 	fn test_solution() {
 		let input = String::from(r"125 17");
-		let answer = solution(&input, 6);
+		let answer = solution(&input, 6, 20);
 		assert_eq!(answer, 22);
 	}
 
 	#[test]
 	fn test_solution2() {
 		let input = String::from(r"125 17");
-		let answer = solution(&input, 25);
+		let answer = solution(&input, 25, 20);
 		assert_eq!(answer, 55312);
 	}
 }
 
 fn main() {
 	let input = "3028 78 973951 5146801 5 0 23533 857";
-	let answer = solution(&input, 25);
+	let answer = solution(&input, 25, 30);
 	println!("Answer task 1: {}", answer);
-	let answer = solution(&input, 75);
+	let answer = solution(&input, 75, 20);
 	println!("Answer task 2: {}", answer);
 }
