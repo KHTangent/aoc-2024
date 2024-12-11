e5ec77128c088d991b6c16823624f4ebbebb345d
KHTangent <contact@khtangent.com>
2024-12-11 09:07:04 +0100

    Made day 11 more compact

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,47 +1,28 @@
 use memoize::memoize;
 
 #[memoize]
-fn process_stone(input: i64, blinks: usize, stones_per_recursion: usize) -> i64 {
-	let mut generations_processed = 0;
-	let mut stones = vec![input];
-	for _ in 0..blinks.min(stones_per_recursion) {
-		let mut new_stones: Vec<i64> = Vec::with_capacity(stones.len() * 2);
-		for stone in stones {
-			let stone_string = stone.to_string();
-			if stone == 0 {
-				new_stones.push(1);
-			} else if stone_string.len() % 2 == 0 {
-				let (first, second) = stone_string.split_at(stone_string.len() / 2);
-				new_stones.push(first.parse().unwrap());
-				new_stones.push(second.parse().unwrap());
-			} else {
-				new_stones.push(stone * 2024);
-			}
-		}
-		stones = new_stones;
-		generations_processed += 1;
+fn process_stone_compact(stone: i64, blinks: usize) -> i64 {
+	if blinks == 0 {
+		return 1;
 	}
-	if generations_processed < blinks {
-		stones
-			.into_iter()
-			.map(|stone| {
-				solution(
-					stone.to_string().as_str(),
-					blinks - stones_per_recursion,
-					stones_per_recursion,
-				)
-			})
-			.sum::<i64>()
+	let stone_string = stone.to_string();
+	if stone == 0 {
+		process_stone_compact(1, blinks - 1)
+	} else if blinks != 0 && stone_string.len() % 2 == 0 {
+		let (first, second) = stone_string.split_at(stone_string.len() / 2);
+		let first_result = process_stone_compact(first.parse().unwrap(), blinks - 1);
+		let second_result = process_stone_compact(second.parse().unwrap(), blinks - 1);
+		first_result + second_result
 	} else {
-		stones.len() as i64
+		process_stone_compact(stone * 2024, blinks - 1)
 	}
 }
 
-fn solution(input: &str, blinks: usize, stones_per_recursion: usize) -> i64 {
+fn solution(input: &str, blinks: usize) -> i64 {
 	input
 		.split(" ")
 		.map(|s| s.parse::<i64>().unwrap())
-		.map(|stone| process_stone(stone, blinks, stones_per_recursion))
+		.map(|stone| process_stone_compact(stone, blinks))
 		.sum::<i64>()
 }
 
@@ -52,22 +33,22 @@ mod tests {
 	#[test]
 	fn test_solution() {
 		let input = String::from(r"125 17");
-		let answer = solution(&input, 6, 20);
+		let answer = solution(&input, 6);
 		assert_eq!(answer, 22);
 	}
 
 	#[test]
 	fn test_solution2() {
 		let input = String::from(r"125 17");
-		let answer = solution(&input, 25, 20);
+		let answer = solution(&input, 25);
 		assert_eq!(answer, 55312);
 	}
 }
 
 fn main() {
 	let input = "3028 78 973951 5146801 5 0 23533 857";
-	let answer = solution(&input, 25, 30);
+	let answer = solution(&input, 25);
 	println!("Answer task 1: {}", answer);
-	let answer = solution(&input, 75, 20);
+	let answer = solution(&input, 75);
 	println!("Answer task 2: {}", answer);
 }
