556d80d8d07ea5b196b1bfcafa773b818edf0efa
KHTangent <contact@khtangent.com>
2024-12-14 13:21:35 +0100

    Complete day 14

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -60,13 +60,29 @@ fn solution(input: &String, map_width: i64, map_height: i64) -> i64 {
 	quadrant_scores.0 * quadrant_scores.1 * quadrant_scores.2 * quadrant_scores.3
 }
 
-fn solution2(input: &String) -> i64 {
-	let mut sum = 0;
-	for line in input.lines() {
-		let num = line.parse::<i64>().unwrap_or(0);
-		sum += num;
+fn robots_to_string(input: &Vec<Robot>, map_width: i64, map_height: i64) -> String {
+	let mut strings = vec![String::from(".").repeat(map_width as usize); map_height as usize];
+	for robot in input.iter() {
+		unsafe {
+			strings[robot.position.1 as usize].as_bytes_mut()[robot.position.0 as usize] =
+				'#' as u8;
+		}
 	}
-	sum
+	strings.join("\n")
+}
+
+fn solution2(input: &String, map_width: i64, map_height: i64, iterations: i64) -> i64 {
+	let mut robots: Vec<Robot> = input.lines().map(|s| s.parse::<Robot>().unwrap()).collect();
+	for i in 0..iterations {
+		robots
+			.iter_mut()
+			.for_each(|r| r.move_once(map_width, map_height));
+		let printable = robots_to_string(&robots, map_width, map_height);
+		if printable.contains("########") {
+			return i+1;
+		}
+	}
+	0
 }
 
 #[cfg(test)]
@@ -93,17 +109,6 @@ p=9,5 v=-3,-3
 		let answer = solution(&input, 11, 7);
 		assert_eq!(answer, 12);
 	}
-
-	#[test]
-	fn test_solution2() {
-		let input = String::from(
-			r"2
-4
-",
-		);
-		let answer = solution2(&input);
-		assert_eq!(answer, 6);
-	}
 }
 
 fn get_entire_input_file() -> String {
@@ -116,6 +121,6 @@ fn main() {
 	let file = get_entire_input_file();
 	let answer = solution(&file, 101, 103);
 	println!("Answer task 1: {}", answer);
-	let answer = solution2(&file);
+	let answer = solution2(&file, 101, 103, 10000);
 	println!("Answer task 2: {}", answer);
 }
